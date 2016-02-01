e0aec32b70f65e564b5370f272e722e53fbc0497
Tim Flichy <timothee.flichy@mail.mcgill.ca>
2016-02-01 13:53:46 -0500

    Update naming

--- modelsim_test/g47_lab1.vhd
+++ modelsim_test/g47_lab1.vhd
@@ -1,15 +1,15 @@
 -- Copyright (C) 1991-2013 Altera Corporation
--- Your use of Altera Corporation's design tools, logic functions 
--- and other software and tools, and its AMPP partner logic 
--- functions, and any output files from any of the foregoing 
--- (including device programming or simulation files), and any 
--- associated documentation or information are expressly subject 
--- to the terms and conditions of the Altera Program License 
--- Subscription Agreement, Altera MegaCore Function License 
--- Agreement, or other applicable license agreement, including, 
--- without limitation, that your use is for the sole purpose of 
--- programming logic devices manufactured by Altera and sold by 
--- Altera or its authorized distributors.  Please refer to the 
+-- Your use of Altera Corporation's design tools, logic functions
+-- and other software and tools, and its AMPP partner logic
+-- functions, and any output files from any of the foregoing
+-- (including device programming or simulation files), and any
+-- associated documentation or information are expressly subject
+-- to the terms and conditions of the Altera Program License
+-- Subscription Agreement, Altera MegaCore Function License
+-- Agreement, or other applicable license agreement, including,
+-- without limitation, that your use is for the sole purpose of
+-- programming logic devices manufactured by Altera and sold by
+-- Altera or its authorized distributors.  Please refer to the
 -- applicable agreement for further details.
 
 -- PROGRAM		"Quartus II 32-bit"
@@ -17,11 +17,11 @@
 -- CREATED		"Fri Jan 15 14:45:28 2016"
 
 LIBRARY ieee;
-USE ieee.std_logic_1164.all; 
+USE ieee.std_logic_1164.all;
 
 LIBRARY work;
 
-ENTITY g47_lab1 IS 
+ENTITY g47_lab1 IS
 	PORT
 	(
 		A :  IN  STD_LOGIC_VECTOR(5 DOWNTO 0);
@@ -30,7 +30,7 @@ ENTITY g47_lab1 IS
 	);
 END g47_lab1;
 
-ARCHITECTURE bdf_type OF g47_lab1 IS 
+ARCHITECTURE bdf_type OF g47_lab1 IS
 
 COMPONENT g47_comp6
 	PORT(A : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
@@ -41,7 +41,7 @@ END COMPONENT;
 
 
 
-BEGIN 
+BEGIN
 
 
 
