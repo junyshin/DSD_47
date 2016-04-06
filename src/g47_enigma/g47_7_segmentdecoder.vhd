711a9c5641f0ad2974cc7c200cd5ebdbc167a833
Tim Flichy <timothee.flichy@mail.mcgill.ca>
2016-04-06 14:32:48 -0400

    Update display for digits

--- src/g47_enigma/g47_7_segmentdecoder.vhd
+++ src/g47_enigma/g47_7_segmentdecoder.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity g47_7_segmentdecoder is
   port (
+    segment_type: in std_logic;
     index: in std_logic_vector(4 downto 0);
     segments: out std_logic_vector(6 downto 0)
   ) ;
@@ -11,33 +12,55 @@ end entity ; -- g47_7_segmentdecoder
 architecture arch of g47_7_segmentdecoder is
   signal tmp: std_logic_vector(6 downto 0);
 begin
-  with index select
-    tmp <= "1110111" when "00000", --A
-           "1111100" when "00001", --B
-           "0111001" when "00010", --C
-           "1011110" when "00011", --D
-           "1111001" when "00100", --E
-           "1110001" when "00101", --F
-           "0111101" when "00110", --G
-           "1110100" when "00111", --H
-           "0110000" when "01000", --I
-           "0011110" when "01001", --J
-           "1110000" when "01010", --K
-           "0111000" when "01011", --L
-           "0110111" when "01100", --M
-           "1010100" when "01101", --N
-           "0111111" when "01110", --O
-           "1110011" when "01111", --P
-           "1011100" when "10000", --Q
-           "0110001" when "10001", --R
-           "1101101" when "10010", --S
-           "1111000" when "10011", --T
-           "0111110" when "10100", --U
-           "0100110" when "10101", --V
-           "0011100" when "10110", --W
-           "1110110" when "10111", --X
-           "1101110" when "11000", --Y
-           "1011011" when "11001", --Z
-           "0000000" when others; --Error
+  DISPLAY : process( segment_type, index )
+  begin
+    case( segment_type ) is
+      when '0' => -- Letters
+        case( index ) is
+          when "00000" => tmp <= "1110111"; -- A
+          when "00001" => tmp <= "1111100"; -- B
+          when "00010" => tmp <= "0111001"; -- C
+          when "00011" => tmp <= "1011110"; -- D
+          when "00100" => tmp <= "1111001"; -- E
+          when "00101" => tmp <= "1110001"; -- F
+          when "00110" => tmp <= "0111101"; -- G
+          when "00111" => tmp <= "1110100"; -- H
+          when "01000" => tmp <= "0110000"; -- I
+          when "01001" => tmp <= "0011110"; -- J
+          when "01010" => tmp <= "1110000"; -- K
+          when "01011" => tmp <= "0111000"; -- L
+          when "01100" => tmp <= "0110111"; -- M
+          when "01101" => tmp <= "1010100"; -- N
+          when "01110" => tmp <= "0111111"; -- O
+          when "01111" => tmp <= "1110011"; -- P
+          when "10000" => tmp <= "1011100"; -- Q
+          when "10001" => tmp <= "0110001"; -- R
+          when "10010" => tmp <= "1101101"; -- S
+          when "10011" => tmp <= "1111000"; -- T
+          when "10100" => tmp <= "0111110"; -- U
+          when "10101" => tmp <= "0100110"; -- V
+          when "10110" => tmp <= "0011100"; -- W
+          when "10111" => tmp <= "1110110"; -- X
+          when "11000" => tmp <= "1101110"; -- Y
+          when "11001" => tmp <= "1011011"; -- Z
+          when others  => tmp <= "0000000";
+        end case ;
+      when '1' => -- Digits
+        case( index ) is
+          when "00000" => tmp <= ; -- 0
+          when "00001" => tmp <= ; -- 1
+          when "00010" => tmp <= ; -- 2
+          when "00011" => tmp <= ; -- 3
+          when "00100" => tmp <= ; -- 4
+          when "00101" => tmp <= ; -- 5
+          when "00110" => tmp <= ; -- 6
+          when "00111" => tmp <= ; -- 7
+          when "01000" => tmp <= ; -- 8
+          when "01001" => tmp <= ; -- 9
+          when others  => tmp <= "0000000";
+        end case ;
+      when others => tmp <= "0000000";
+    end case ;
     segments <= not tmp;
+  end process ; -- DISPLAY
 end architecture ; -- arch
